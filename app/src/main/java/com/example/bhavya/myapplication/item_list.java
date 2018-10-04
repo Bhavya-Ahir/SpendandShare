823f0167b925475d8e022bba7603588e54d5ff86
Bhavya-Ahir <42085401+Bhavya-Ahir@users.noreply.github.com>
2018-10-04 12:56:19 +0530

    extra

---
+++ app/src/main/java/com/example/bhavya/myapplication/item_list.java
@@ -0,0 +1,14 @@
+package com.example.bhavya.myapplication;
+
+public class item_list {
+
+    public String name;
+
+    public item_list(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
