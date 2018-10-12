830c44d6168230e14b5ad15831064bebe15cb11b
Harsh <harsh.agarwal@spit.ac.in>
2018-10-12 16:39:40 +0530

    data to database

--- app/src/main/java/com/example/bhavya/myapplication/Settle.java
+++ app/src/main/java/com/example/bhavya/myapplication/Settle.java
@@ -108,7 +108,7 @@ public class Settle extends AppCompatActivity {
             for (int i = 0; i < Settle.name.size(); i++) {
                 list.add(new Person(Settle.name.get(i), Settle.paid.get(i), 0));
                 //dataMap.put(Person.class);
-                dataMap.put("Amount paid " + i, Settle.paid.get(i));
+                //dataMap.put("Amount paid " + i, Settle.paid.get(i));
                 Log.i("grp name", groupName);
                 myDatabase.child(Settle.name.get(i)).child("BILL PAID ").setValue(Settle.paid.get(i));
 
@@ -152,7 +152,7 @@ public class Settle extends AppCompatActivity {
                 Person x = plist.get(i);
                 Person y = nlist.get(j);
                 if ((-1 * y.getBalance()) > x.getBalance()) {
-                    myDatabase.child(Settle.name.get(i)).child("transaction").child(y.getName()).setValue(x.getBalance());
+                    myDatabase.child(x.getName()).child("transaction").child(y.getName()).setValue(x.getBalance());
                     System.out.println(message.add(x.getName() + " will pay Rs :" + x.getBalance() + " to " + y.getName()));
                     y.setBalance(y.getBalance() + x.getBalance());//updating negative list
                     x.setBalance(0);
@@ -160,6 +160,8 @@ public class Settle extends AppCompatActivity {
                     // break outer;
                 }
                 if (x.getBalance() > (-1 * y.getBalance())) {
+                    myDatabase.child(x.getName()).child("transaction").child(y.getName()).setValue(x.getBalance());
+
                     System.out.println(message.add(x.getName() + " will pay Rs:" + (-1 * y.getBalance()) + " to " + y.getName()));
                     x.setBalance(x.getBalance() + y.getBalance());//updating positive list
                     y.setBalance(0);
@@ -168,6 +170,8 @@ public class Settle extends AppCompatActivity {
                 }
 
                 if (x.getBalance() + y.getBalance() == 0) {
+                    myDatabase.child(x.getName()).child("transaction").child(y.getName()).setValue(x.getBalance());
+
 
                     System.out.println(message.add(x.getName() + " will pay Rs :" + x.getBalance() + " to " + y.getName()));
                     x.setBalance(0);
