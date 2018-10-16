908869d497dcbe59f0980daab98bd1c13e469d5b
Bhavya-Ahir <42085401+Bhavya-Ahir@users.noreply.github.com>
2018-10-16 16:10:39 +0530

    trial

--- app/src/main/java/com/example/bhavya/myapplication/GroupData.java
+++ app/src/main/java/com/example/bhavya/myapplication/GroupData.java
@@ -1,13 +1,57 @@
 package com.example.bhavya.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupData extends AppCompatActivity {
-
+    ListView listViewMembers;
+    private DatabaseReference groupData;
+    List<Settle.Person> memberList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_data);
+        listViewMembers=(ListView)findViewById(R.id.memberListViewid);
+        groupData=FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        memberList=new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        groupData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot personSnapshot:dataSnapshot.getChildren()){
+                    Settle.Person p= personSnapshot.getValue(Settle.Person.class);
+                    memberList.add(p);
+                }
+                MemberList adapter=new MemberList(GroupData.this,memberList);
+                listViewMembers.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/bhavya/myapplication/MemberList.java
@@ -0,0 +1,42 @@
+package com.example.bhavya.myapplication;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MemberList extends ArrayAdapter<Settle.Person> {
+    private Activity context;
+    private List<Settle.Person> memberList;
+    public MemberList(Activity context,List<Settle.Person> memberList){
+        super(context,R.layout.datalist_layout,memberList);
+        this.context=context;
+        this.memberList=memberList;
+    }
+
+    @androidx.annotation.NonNull
+    @NonNull
+    @Override
+    public View getView(int position, @androidx.annotation.Nullable  @Nullable View convertView, @androidx.annotation.NonNull @NonNull ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View listViewitem1 = inflater.inflate(R.layout.datalist_layout, null, true);
+            TextView textViewName = (TextView) listViewitem1.findViewById(R.id.memberName);
+            TextView textViewamt = (TextView) listViewitem1.findViewById(R.id.amtPaid);
+            Settle.Person p = memberList.get(position);
+            textViewName.setText(p.getName());
+            textViewamt.setText(p.getName());
+            return listViewitem1;
+            return super.getView(position, convertView, parent);
+        }
+        return super.getView(position, convertView, parent);
+        return v;
+    }
+}
